7d35350220d2bece07698ac7373cf81617ae0d31
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-05-17 17:03:07 +0200

    sync
    Former-commit-id: 4c68e08fe3b4a34cc58c0137c3463bda86c9eb67

--- examples/tcn_causalimpact.py
+++ examples/tcn_causalimpact.py
@@ -1,48 +1,59 @@
+from os import SCHED_BATCH
 import sys
+
+from numpy.core.numeric import allclose
 
 sys.path.append("../src")
 from parse_cesar1 import get_timestep_tuples
 import pickle
 from pyprocessta.causalimpact import _select_unrelated_x
-from pyprocessta.model.tcn import transform_data, run_model, parallelized_inference
-
+from pyprocessta.model.tcn import (
+    transform_data,
+    parallelized_inference,
+    TCNModelDropout,
+)
+from pyprocessta.model.scaler import Scaler
 from darts import TimeSeries
 import pandas as pd
 from copy import deepcopy
 import time
+import numpy as np
 
 TIMESTR = time.strftime("%Y%m%d-%H%M%S")
 
 MEAS_COLUMNS = [
     "TI-19",
-    "FI-16",
-    "TI-33",
-    "FI-2",
-    "FI-151",
-    "TI-8",
-    "FI-241",
-    "valve-position-12",  # dry-bed
-    "FI-38",  # stripper
-    "PI-28",  # stripper
-    "TI-28",  # stripper
-    "FI-20",
-    "FI-30",
+    #      "FI-16",
+    #     "TI-33",
+    #     "FI-2",
+    #     "FI-151",
+    #     "TI-8",
+    #     "FI-241",
+    #  "valve-position-12",  # dry-bed
+    #     "FI-38",  # strippera
+    #     "PI-28",  # stripper
+    #     "TI-28",  # stripper
+    #      "FI-20",
+    #     "FI-30",
     "TI-3",
     "FI-19",
-    "FI-211",
+    #     "FI-211",
     "FI-11",
-    "TI-30",
-    "PI-30",
+    #     "TI-30",
+    #     "PI-30",
     "TI-1213",
-    "TI-4",
+    #     "TI-4",
     "FI-23",
+    "FI-20",
+    "FI-20/FI-23",
+    #    "TI-22",
     "delta_t",
+    "TI-35",
+    "delta_t_2",
 ]
+HORIZON = 6
 
-df = pd.read_pickle("detrended.pkl")
-df = df.dropna()
-df["delta_t"] = df["TI-35"] - df["TI-4"]
-MEAS_COLUMNS.append("delta_t")
+df = pd.read_pickle("20210508_df_for_causalimpact.pkl")
 
 with open("step_times.pkl", "rb") as handle:
     times = pickle.load(handle)
@@ -51,14 +62,17 @@ with open("step_times.pkl", "rb") as handle:
 def get_causalimpact_splits(x, y, day, times, df):
     a, b = get_timestep_tuples(df, times, day)
 
-    x_way_before, x_after = x.split_before(a[1])
-    y_way_before, y_after = y.split_before(pd.Timestamp(a[1]))
+    x_way_before, x_after = x.split_before(b[1])
+    y_way_before, y_after = y.split_before(pd.Timestamp(b[1]))
 
-    _, x_before = x_way_before.split_before(a[0])
-    _, y_before = y_way_before.split_before(a[0])
+    _, x_before_ = x_way_before.split_before(a[0])
+    _, y_before_ = y_way_before.split_before(a[0])
 
-    x_during, x_test = x_after.split_before(b[1])
-    y_during, y_test = y_after.split_before(b[1])
+    x_before, x_after = x_before_.split_before(a[1])
+    y_before, y_after = y_before_.split_before(a[1])
+
+    x_during, x_test = x_after.split_before(b[0])
+    y_during, y_test = y_after.split_before(b[0])
 
     return (x_before, y_before), (x_during, y_during), (x_test, y_test)
 
@@ -83,71 +97,163 @@ step_changes = [
     ["FI-19"],
     ["TI-3"],
     ["FI-11"],
-    ["FI-11, FI-3"],
+    ["FI-11", "FI-2"],
     ["TI-1213"],
     ["TI-1213", "TI-19"],
     ["capture rate"],
-    ["capture rate"],
-    ["valve-position-12"],
+    # # ["capture rate"],
+    # ["valve-position-12"],
 ]
 
 
 causalimpact_models = {}
 
 
+def run_model(x_trains, y_trains):
+
+    # run = wandb.init(project='process_ml', reinit=True, sync_tensorboard=True)
+    # with run:
+
+    model_cov = TCNModelDropout(
+        input_chunk_length=30,
+        output_chunk_length=HORIZON,
+        num_layers=4,
+        num_filters=64,
+        kernel_size=4,
+        dropout=0.3,
+        weight_norm=True,
+        batch_size=32,
+        n_epochs=200,
+        log_tensorboard=True,
+        # optimizer_kwargs={"lr": 1e-5},
+    )
+
+    model_cov.fit(series=y_trains, covariates=x_trains, verbose=False)
+
+    return model_cov
+
+
+def transform_data_new(train_tuple, test_tuples, all_tuple):
+    x_train, y_train = train_tuple
+
+    transformer = Scaler()
+
+    x_train = transformer.fit_transform(x_train)
+
+    # with open("x_scaler.pkl", "wb") as handle:
+    #     pickle.dump(transformer, handle)
+
+    y_transformer = Scaler(name="YScaler")
+    y_train = y_transformer.fit_transform(y_train)
+
+    transformed_test_tuples = []
+    for x_test, y_test in test_tuples:
+        print(x_test.pd_dataframe().shape, y_test.pd_dataframe().shape)
+        x_test = transformer.transform(x_test)
+        y_test = y_transformer.transform(y_test)
+        transformed_test_tuples.append((x_test, y_test))
+
+    # with open("y_scaler.pkl", "wb") as handle:
+    #     pickle.dump(y_transformer, handle)
+
+    x_all = transformer.transform(all_tuple[0])
+    y_all = transformer.transform(all_tuple[1])
+    return (
+        (x_train, y_train),
+        transformed_test_tuples,
+        (transformer, y_transformer),
+        (x_all, y_all),
+    )
+
+
 if __name__ == "__main__":
-    TARGETS = ["2-Amino-2-methylpropanol C4H11NO", "Piperazine C4H10N2"]
-    for day in range(10):
+    #TARGETS = ["2-Amino-2-methylpropanol C4H11NO", "Piperazine C4H10N2"]
+    TARGETS = ["Carbon dioxide CO2", "Ammonia NH3"]
+    for day in range(len(step_changes)):
         cols = deepcopy(MEAS_COLUMNS)
         if step_changes[day][0] in MEAS_COLUMNS:
             for var in step_changes[day]:
                 try:
-                    cols = _select_unrelated_x(df, cols, var, 0.05)
-                    y = TimeSeries.from_dataframe(df, value_cols=TARGETS)
-                    x = TimeSeries.from_dataframe(df, value_cols=cols)
+                    cols = _select_unrelated_x(df, cols, var, 0.01)
+                    y = TimeSeries.from_dataframe(df[TARGETS])
+                    x = TimeSeries.from_dataframe(df[cols])
+
+                    x = Scaler().fit_transform(x)
+                    y = Scaler().fit_transform(y)
+
+                    x_trains = []
+                    y_trains = []
+
+                    # for day_ in range(len(step_changes)):
+                    #     before, during, after = get_causalimpact_splits(
+                    #         x, y, day_, times, df
+                    #     )
+
+                    #     x_trains.append(before[0])
+                    #     y_trains.append(before[1])
 
                     before, during, after = get_causalimpact_splits(
                         x, y, day, times, df
                     )
-                    train_tuple, test_tuple, scalers = transform_data(
-                        before, [during, after]
+
+                    x_trains.append(before[0])
+                    y_trains.append(before[1])
+
+                    before_x_df, before_y_df = (
+                        before[0].pd_dataframe(),
+                        before[1].pd_dataframe(),
                     )
-                    model = run_model(train_tuple)
+                    during_x_df, during_y_df = (
+                        during[0].pd_dataframe(),
+                        during[1].pd_dataframe(),
+                    )
+                    after_x_df, after_y_df = (
+                        after[0].pd_dataframe(),
+                        after[1].pd_dataframe(),
+                    )
+
+                    day_x_df = pd.concat([before_x_df, during_x_df, after_x_df], axis=0)
+                    day_x_ts = TimeSeries.from_dataframe(day_x_df)
+
+                    day_y_df = pd.concat([before_y_df, during_y_df, after_y_df], axis=0)
+                    day_y_ts = TimeSeries.from_dataframe(day_y_df)
+
+                    model = run_model(x_trains, y_trains)
 
                     causalimpact_models[day] = {
                         "before": before,
                         "during": during,
                         "after": after,
                         "model": model,
+                        "x_all": day_x_ts,
+                        "y_all": day_y_ts,
+                        "cols": cols,
                     }
-                except Exception:
+                except Exception as e:
+                    print(e)
                     pass
 
     for day, values in causalimpact_models.items():
         if step_changes[day][0] in MEAS_COLUMNS:
             try:
-                x_test, y_test = values["before"]
                 model = values["model"]
-                before_predictions = parallelized_inference(
-                    model, x_test, y_test, start=0.2
+                predictions = parallelized_inference(
+                    model,
+                    values["x_all"],
+                    values["y_all"],
+                    start=(len(values["before"][0]) - 0.4 * len(values["before"][0]))
+                    / len(values["y_all"]),
+                    repeats=10,
+                    horizon=HORIZON,
                 )
 
-                x_test, y_test = values["during"]
-                during_predictions = parallelized_inference(
-                    model, x_test, y_test, start=0.2
-                )
-
-                x_test, y_test = values["after"]
-                after_predictions = parallelized_inference(
-                    model, x_test, y_test, start=0.2
-                )
-
-                causalimpact_models[day]["before_predictions"] = before_predictions
-                causalimpact_models[day]["during_predictions"] = during_predictions
-                causalimpact_models[day]["after_predictions"] = after_predictions
+                causalimpact_models[day]["predictions"] = predictions
             except Exception as e:
                 print(e)
                 pass
 
-    with open(f"{TIMESTR}-causalimpact_2amp_pip.pkl", "wb") as handle:
-        pickle.dump(causalimpact_models, handle)
+        with open(
+            f"{TIMESTR}-_{day}_causalimpact_{'_'.join(TARGETS)}.pkl".replace("/", "*"),
+            "wb",
+        ) as handle:
+            pickle.dump(causalimpact_models, handle)
