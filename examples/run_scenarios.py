badba93ed2d3b54bfdc2e2c4815a54b47243b8d3
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-05-17 17:03:07 +0200

    sync
    Former-commit-id: 4c68e08fe3b4a34cc58c0137c3463bda86c9eb67

--- examples/run_scenarios.py
+++ examples/run_scenarios.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 import pickle
-from pyprocessta.model.tcn import parallelized_inference, summarize_results
+from pyprocessta.model.tcn import (
+    parallelized_inference,
+    summarize_results,
+    TCNModelDropout,
+)
 import pandas as pd
 from darts import TimeSeries
 from copy import deepcopy
@@ -8,6 +12,10 @@ import numpy as np
 import joblib
 import click
 import time
+import os
+
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def load_pickle(filename):
@@ -23,28 +31,33 @@ def dump_pickle(object, filename):
 
 MEAS_COLUMNS = [
     "TI-19",
-    "FI-16",
-    "TI-33",
-    "FI-2",
-    "FI-151",
-    "TI-8",
-    "FI-241",
-    "valve-position-12",  # dry-bed
-    "FI-38",  # stripper
-    "PI-28",  # stripper
-    "TI-28",  # stripper
-    "FI-20",
-    "FI-30",
+    #      "FI-16",
+    #     "TI-33",
+    #     "FI-2",
+    #     "FI-151",
+    #     "TI-8",
+    #     "FI-241",
+    #  "valve-position-12",  # dry-bed
+    #     "FI-38",  # strippera
+    #     "PI-28",  # stripper
+    #     "TI-28",  # stripper
+    #      "FI-20",
+    #     "FI-30",
     "TI-3",
     "FI-19",
-    "FI-211",
+    #     "FI-211",
     "FI-11",
-    "TI-30",
-    "PI-30",
+    #     "TI-30",
+    #     "PI-30",
     "TI-1213",
-    "TI-4",
+    #     "TI-4",
     "FI-23",
+    "FI-20",
+    "FI-20/FI-23",
+    #    "TI-22",
     "delta_t",
+    "TI-35",
+    "delta_t_2",
 ]
 
 # First, we train a model on *all* data
@@ -52,24 +65,58 @@ MEAS_COLUMNS = [
 
 # load the trained model
 
+model_cov1 = TCNModelDropout(
+    input_chunk_length=8,
+    output_chunk_length=1,
+    num_layers=5,
+    num_filters=16,
+    kernel_size=6,
+    dropout=0.3,
+    weight_norm=True,
+    batch_size=32,
+    n_epochs=100,
+    log_tensorboard=True,
+    optimizer_kwargs={"lr": 2e-4},
+)
+
+
+model_cov2 = TCNModelDropout(
+    input_chunk_length=8,
+    output_chunk_length=1,
+    num_layers=5,
+    num_filters=16,
+    kernel_size=6,
+    dropout=0.3,
+    weight_norm=True,
+    batch_size=32,
+    n_epochs=100,
+    log_tensorboard=True,
+    optimizer_kwargs={"lr": 2e-4},
+)
+
+
 FEAT_NUM_MAPPING = dict(zip(MEAS_COLUMNS, [str(i) for i in range(len(MEAS_COLUMNS))]))
 UPDATE_MAPPING = {
     "amine": {
-        "scaler": joblib.load("y_transformer_2amp_pip"),
-        "model": joblib.load("2amp_pip_model"),
+        "scaler": joblib.load("y_transformer__reduced_feature_set"),
+        "model": model_cov1.load_from_checkpoint(
+            os.path.join(THIS_DIR, "2amp_pip_model_reduced_feature_set_darts")
+        ),
         "name": ["2-Amino-2-methylpropanol C4H11NO", "Piperazine C4H10N2"],
     },
     "co2": {
-        "scaler": joblib.load("y_transformer_co2_ammonia"),
-        "model": joblib.load("co2_ammonia_model"),
+        "scaler": joblib.load("y_transformer_co2_ammonia_reduced_feature_set"),
+        "model": model_cov2.load_from_checkpoint(
+            os.path.join(THIS_DIR, "co2_ammonia_model_reduced_feature_set_darts")
+        ),
         "name": ["Carbon dioxide CO2", "Ammonia NH3"],
     },
 }
-SCALER = joblib.load("x_scaler")
+SCALER = joblib.load("x_scaler_reduced_feature_set")
 
 
 # load data
-DF = pd.read_pickle("df_dropped_5min_resampled.pkl")
+DF = pd.read_pickle("20210508_df_cleaned.pkl")
 
 
 def run_update(x, target="amine"):
@@ -77,7 +124,7 @@ def run_update(x, target="amine"):
     y = model_dict["scaler"].transform(
         TimeSeries.from_dataframe(DF, value_cols=model_dict["name"])
     )
-    df = parallelized_inference(model_dict["model"], x, y, repeats=10)
+    df = parallelized_inference(model_dict["model"], x, y, repeats=2)
     means, stds = summarize_results(df)
     return {"means": means, "stds": stds}
 
@@ -87,7 +134,10 @@ def run_targets(feature_levels, target: str = "amine"):
     df = deepcopy(DF)
 
     for k, v in feature_levels.items():
-        df[k] = df[k] + v / 100 * np.abs(df[k])
+        if k == "valve-position-12":
+            df[k] = v
+        else:
+            df[k] = df[k] + v / 100 * np.abs(df[k])
 
     X_ = TimeSeries.from_dataframe(df, value_cols=MEAS_COLUMNS)
     X_ = SCALER.transform(X_)
@@ -101,13 +151,24 @@ def run_grid(
     feature_b: str = "FI-19",
     lower: float = -20,
     upper: float = 20,
-    num_points: int = 21,
+    num_points: int = 4,
     objectives: str = "amine",
 ):
     grid = np.linspace(lower, upper, num_points)
     results_double_new = defaultdict(dict)
-    for point_a in grid:
-        for point_b in grid:
+
+    if feature_a == "valve-position-12":
+        grid_a = [0, 1]
+    else:
+        grid_a = grid
+    if feature_b == "valve-position-12":
+        grid_b = [0, 1]
+    else:
+        grid_b = grid
+
+    for point_a in grid_a:
+        for point_b in grid_b:
+            print(f"Running point {feature_a}: {point_a} {feature_b}: {point_b}")
             results_double_new[point_a][point_b] = run_targets(
                 {feature_a: point_a, feature_b: point_b}, objectives
             )
@@ -127,7 +188,9 @@ def main(feature_a, feature_b, lower, upper, num_points, objectives):
     results = run_grid(feature_a, feature_b, lower, upper, num_points, objectives)
     dump_pickle(
         results,
-        f"{TIMESTR}_{feature_a}_{feature_b}_{lower}_{upper}_{num_points}_{objectives}",
+        f"{TIMESTR}_{feature_a}_{feature_b}_{lower}_{upper}_{num_points}_{objectives}_new".replace(
+            "/", "*"
+        ),
     )
 
 
